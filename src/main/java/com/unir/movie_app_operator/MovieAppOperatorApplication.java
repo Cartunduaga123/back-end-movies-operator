9aa1806a959f7a8e17a519404f503547c6821c11
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-06-02 15:54:18 -0500

    First Commit

---
+++ src/main/java/com/unir/movie_app_operator/MovieAppOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.unir.movie_app_operator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/movie_app_operator/MovieAppOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.movie_app_operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
