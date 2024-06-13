34fe1ac19dfc373e4099d4cbffd083e941821c98
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2024-06-12 20:36:14 -0500

    remove unused files

--- src/main/java/com/unir/movie_app_operator/model/dto/BaseResponse.java
+++
@@ -1,7 +0,0 @@
-package com.unir.movie_app_operator.model.dto;
-
-public record BaseResponse(String[] errorMessages) {
-    public boolean hasErrors() {
-        return errorMessages != null && errorMessages.length > 0;
-    }
-}
--- src/main/java/com/unir/movie_app_operator/model/dto/OrderItemsResponse.java
+++
@@ -1,4 +0,0 @@
-package com.unir.movie_app_operator.model.dto;
-
-public record OrderItemsResponse(Long id, String sku, Double price, Long quantity) {
-}
