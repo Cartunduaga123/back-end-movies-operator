5b0873c7cdc6a9f36ee04a8b8c982b2e455d8187
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-07-06 08:11:26 -0500

    commit

--- src/main/java/com/unir/movie_app_operator/MovieAppOperatorApplication.java
+++ src/main/java/com/unir/movie_app_operator/MovieAppOperatorApplication.java
@@ -1,13 +1,17 @@
 package com.unir.movie_app_operator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableDiscoveryClient
+@Slf4j
+
 public class MovieAppOperatorApplication {
 
 	public static void main(String[] args) {
